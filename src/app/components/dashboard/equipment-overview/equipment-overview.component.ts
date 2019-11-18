7b5a0882c4eccb75576cccb070fcea903b5134ac
Julia Engleder <julia@engleder.eu>
2019-11-18 10:31:23 +0100

    button change event

--- src/app/components/dashboard/equipment-overview/equipment-overview.component.ts
+++ src/app/components/dashboard/equipment-overview/equipment-overview.component.ts
@@ -6,118 +6,143 @@ import {HttpService} from '../../../../../services/services/http.service';
 import {DatePipe} from '@angular/common';
 
 export interface DialogData {
-    interneNummer?: string;
-    category?: string;
-    name?: string;
-    brand?: string;
-    displayname?: string;
-    serialNumber?: string;
-    usableClasses?: string[];
-    price?: number;
-    photoPath?: string;
-    specs?: string[];
+  interneNummer?: string;
+  category?: string;
+  name?: string;
+  brand?: string;
+  displayname?: string;
+  serialNumber?: string;
+  usableClasses?: string[];
+  price?: number;
+  photoPath?: string;
+  specs?: string[];
 }
 
 @Component({
-    selector: 'EquipmentOverview',
-    templateUrl: './equipment-overview.component.html',
-    styleUrls: ['./equipment-overview.component.css']
+  selector: 'EquipmentOverview',
+  templateUrl: './equipment-overview.component.html',
+  styleUrls: ['./equipment-overview.component.css']
 })
 export class EquipmentOverviewComponent implements OnInit {
 
 
-    equipment: Equipment[];
+  equipment: Equipment[];
 
 
-    constructor(public equService: DataService, public dialog: MatDialog, private httpService: HttpService) {
+  constructor(public equService: DataService, public dialog: MatDialog, private httpService: HttpService) {
+  }
+
+  ngOnInit() {
+    this.httpService.fetchAllEquipment().subscribe(res => {
+      this.equipment = res;
+    });
+    /* for (const equ of this.equipment) {
+         equ.setDisplayName();
+     }*/
+  }
+
+  choosePhotoPath(cat: string) {
+    switch (cat) {
+      case 'video':
+        return '../assets/icons/equip/videocamera_icon.svg';
+      case 'camera':
+        return '../assets/icons/equip/camera_icon.svg';
+      case 'audio':
+        return '../assets/icons/equip/microphone_icon.svg';
+      default:
+        return '../assets/LULULU.png';
     }
+  }
 
-    ngOnInit() {
-        this.httpService.fetchAllEquipment().subscribe(res => {
-            this.equipment = res;
-        });
-        /* for (const equ of this.equipment) {
-             equ.setDisplayName();
-         }*/
-    }
-
-    choosePhotoPath(cat: string) {
-        switch (cat) {
-            case 'video':
-                return '../assets/icons/equip/videocamera_icon.svg';
-            case 'camera':
-                return '../assets/icons/equip/camera_icon.svg';
-            case 'audio':
-                return '../assets/icons/equip/microphone_icon.svg';
-            default:
-                return '../assets/LULULU.png';
-        }
-    }
-
-    openDialog(equi: Equipment) {
+  openDialog(equi: Equipment) {
 
 
-        const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
-            width: '100%',
-            height: '50%',
-            data: {brand: equi.brand, name: equi.name, specs: equi.specs, interneNummer: equi.internenummer, serialNumber: equi.serialnumber, cat: equi.category, usableCla: equi.usableclasses, equip: equi}
-        });
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '100px',
+      height: '50px',
+      data: {
+        brand: equi.brand,
+        name: equi.name,
+        specs: equi.specs,
+        interneNummer: equi.internenummer,
+        serialNumber: equi.serialnumber,
+        cat: equi.category,
+        usableCla: equi.usableclasses,
+        equip: equi
+      }
+    });
 
-    }
+  }
 
 }
 
 @Component({
-    selector: 'dialog-overview-example-dialog',
-    templateUrl: 'dialog-overview-example-dialog.html',
-    styleUrls: ['dialog.css']
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+  styleUrls: ['dialog.css']
 })
 
 export class DialogOverviewExampleDialog {
 
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    public dataservice: DataService, private http: HttpService, public datepipe: DatePipe) {
+  }
 
-    selectedBeginnDate = new Date();
-    selectedEndDate = new Date();
 
-    rentRequest: String = '';
-    fromDate: string = '';
-    toDate: string = '';
-    serialNumber;
-    userid;
-    minDate = new Date();
+  selectedBeginnDate = new Date();
+  selectedEndDate = new Date();
 
-    close(): void {
-        this.dialogRef.close();
+  rentRequest: String = '';
+  fromDate = '';
+  toDate = '';
+  serialNumber;
+  userid;
+  minDate = new Date();
+  isReservieren: Boolean = false;
+  isAusborgen: Boolean = false;
+
+  maxDate = new Date(3000, 0, 1);
+
+  close(): void {
+    this.dialogRef.close();
+  }
+
+  delete(equi: Equipment) {
+    this.dialogRef.close();
+  }
+
+  rent(serialNumber: any, selectedBeginnDate: Date, selectedEndDate: Date): void {
+
+
+    this.toDate = this.datepipe.transform(this.selectedEndDate, 'dd-MM-yyyy');
+    this.fromDate = this.datepipe.transform(this.selectedBeginnDate, 'dd-MM-yyyy');
+    this.serialNumber = serialNumber;
+    // Unique identifier (e.g it150178)
+    this.userid = this.dataservice.sessionUser.username;
+
+    this.http.rentEquipment(this.userid, this.serialNumber, this.fromDate, this.toDate).subscribe(res => console.log(res));
+    this.dialogRef.close();
+  }
+
+  somethingChanged(): void {
+
+    if (this.selectedBeginnDate = this.minDate) {
+this.isAusborgen = true;
+this.isReservieren = false;
+    } else {
+      this.isAusborgen = false;
+      this.isReservieren = true;
     }
 
-    maxDate = new Date(3000, 0, 1);
-
-    delete(equi: Equipment) {
-        this.dialogRef.close();
-    }
-
-    constructor(
-        public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
-        @Inject(MAT_DIALOG_DATA) public data: DialogData,
-        public dataservice: DataService, private http: HttpService, public datepipe: DatePipe) {
-    }
-
-    rent(serialNumber: any, selectedBeginnDate: Date, selectedEndDate: Date): void {
+  }
 
 
-        this.toDate = this.datepipe.transform(this.selectedEndDate, 'dd-MM-yyyy');
-        this.fromDate = this.datepipe.transform(this.selectedBeginnDate, 'dd-MM-yyyy');
-        this.serialNumber = serialNumber;
-        // Unique identifier (e.g it150178)
-        this.userid = this.dataservice.sessionUser.username;
 
-        this.http.rentEquipment(this.userid, this.serialNumber, this.fromDate, this.toDate).subscribe(res => console.log(res));
-        this.dialogRef.close();
-    }
-
-    myFilter = (d: Date): boolean => {
-        const day = d.getDay();
-        // Prevent Saturday and Sunday
-        return day !== 0 && day !== 6;
-    };
+  myFilter = (d: Date): boolean => {
+    const day = d.getDay();
+    // Prevent Saturday and Sunday
+    return day !== 0 && day !== 6;
+  }
 }
